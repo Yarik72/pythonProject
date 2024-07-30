26cf96f6b946c80fd7414bfbe431efcc6c5a5c28
Yarik72 <lazutinyaroslav2021@gmail.com>
2024-07-30 21:27:43 +0500

    Add files via upload

---
+++ module_9_3.py
@@ -0,0 +1,7 @@
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+first_result = (len(f[0]) - len(f[1]) for f in zip(first, second) if len(f[0]) != len(f[1]))
+second_result = (len(first[i]) == len(second[i]) for i in range(len(first)))
+print(list(first_result))
+print(list(second_result))
