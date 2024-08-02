152224a9280e197f18d71b9349c6167ecded4866
Yarik72 <lazutinyaroslav2021@gmail.com>
2024-08-02 22:49:57 +0500

    Add files via upload

---
+++ module_9_7.py
@@ -0,0 +1,23 @@
+def is_prime(func):
+    def wrapper(*args):
+        number = func(*args)
+        for i in range(2, number):
+            if number % i == 0:
+                print("Составное")
+                break
+        else:
+            print("Простое")
+
+        return number
+
+    return wrapper
+
+
+@is_prime
+def sum_three(*args):
+    return sum(args)
+
+
+result = sum_three(2, 3, 6)
+print(result)
+
